9e9a21bfdce1f16071c1edb64e3d750fd19aabb9
taíssa <ol.taissa.2002@gmail.com>
2025-01-05 10:09:06 -0300

    Adiciona funções para preservar o atriubuto nome

--- ex02/DCC192-2024.3/ex04/src/main/java/edu/dcc192/ex03/HomeController.java
+++ ex02/DCC192-2024.3/ex04/src/main/java/edu/dcc192/ex03/HomeController.java
@@ -7,14 +7,18 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomeController { 
+@SessionAttributes("usuario") // Indica que o atributo "usuario" será armazenado na sessão
+public class HomeController {
 
     @Autowired
     private GeradorSenha geradorSenha;
@@ -22,7 +26,7 @@ public class HomeController {
     private String captchaAtual;
 
     @RequestMapping("/")
-    public ModelAndView home(){
+    public ModelAndView home() {
         ModelAndView mv = new ModelAndView("captcha.html");
         captchaAtual = geradorSenha.GerarSenha(); // Gera a senha do CAPTCHA
         mv.addObject("senha", captchaAtual); // Passa a senha para a página
@@ -38,28 +42,48 @@ public class HomeController {
             mv.setViewName("captcha.html");
             mv.addObject("error", "CAPTCHA incorreto. Tente novamente.");
             // Gera um novo CAPTCHA
-            captchaAtual = geradorSenha.GerarSenha(); 
+            captchaAtual = geradorSenha.GerarSenha();
             mv.addObject("senha", captchaAtual); // Passa a nova senha para a página
         }
         return mv;
     }
 
-    @GetMapping("/menu")
-    public ModelAndView getMethodName(@RequestParam(required = false) String name) {
+    @PostMapping("/login")
+    public ModelAndView login(@RequestParam String name, Model model) {
+        // Cria um Map para representar o usuário
+        Map<String, String> usuario = new HashMap<>();
+        usuario.put("nome", name); // Armazena o nome do usuário no Map
+    
+        // Armazena o usuário na sessão
+        model.addAttribute("usuario", usuario);
+    
+        // Redireciona para a página inicial
+        return new ModelAndView("redirect:/index");
+    }
+
+    @GetMapping("/index")
+    public ModelAndView index(@ModelAttribute("usuario") Map<String, String> usuario) {
         ModelAndView mv = new ModelAndView("index");
-        if (name != null) {
-            mv.addObject("userName", name); // Passa o nome como parâmetro para a página
+
+        // Verifica se o usuário está na sessão
+        if (usuario != null && usuario.containsKey("nome")) {
+            String nomeUsuario = usuario.get("nome");
+            mv.addObject("userName", nomeUsuario);
+        } else {
+            mv.addObject("userName", "Visitante"); // Valor padrão se o usuário não estiver na sessão
         }
+
         // Gera 3 senhas diferentes para o CAPTCHA
         List<String> senhasCaptcha = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             senhasCaptcha.add(geradorSenha.GerarSenha());
         }
-        mv.addObject("captchaOptions", senhasCaptcha); // Passa as senhas para a página
+        mv.addObject("captchaOptions", senhasCaptcha);
+
         return mv;
     }
 
-    @GetMapping("captcha")
+    @GetMapping("/captcha")
     public ModelAndView captcha(@RequestParam String captchaOption) {
         ModelAndView mv = new ModelAndView("captcha.html");
         mv.addObject("senha", captchaOption); // Passa a senha selecionada para a página
@@ -67,11 +91,11 @@ public class HomeController {
         return mv;
     }
 
-    @GetMapping("logout")
+    @GetMapping("/logout")
     public ModelAndView getMethodLogout() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("login");
-        mv.addObject("logout", true); 
+        mv.addObject("logout", true);
         return mv;
     }
 
@@ -79,15 +103,9 @@ public class HomeController {
     public ModelAndView bootstrap() {
         return new ModelAndView("bootstrap.html");
     }
-    
+
     @GetMapping("/info")
     public ModelAndView info() {
         return new ModelAndView("info.html");
-    }
-
-    @GetMapping("/index")
-    public ModelAndView index() {
-        ModelAndView mv = new ModelAndView("index");
-        return mv;
     }
 }
