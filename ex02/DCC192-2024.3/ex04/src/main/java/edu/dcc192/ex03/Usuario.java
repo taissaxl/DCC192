31218786cdb209b2850e3c071bbbff61e24f02f4
taíssa <ol.taissa.2002@gmail.com>
2025-01-05 10:09:22 -0300

    Cria classe de usuário

---
+++ ex02/DCC192-2024.3/ex04/src/main/java/edu/dcc192/ex03/Usuario.java
@@ -0,0 +1,13 @@
+package edu.dcc192.ex03;
+
+public class Usuario {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
