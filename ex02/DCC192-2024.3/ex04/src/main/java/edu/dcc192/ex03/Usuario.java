b0ce65d439c42c44006d86f3514b1475b8784065
taíssa <ol.taissa.2002@gmail.com>
2025-01-09 14:48:02 -0300

    adiciona funções na classe usuário

--- ex02/DCC192-2024.3/ex04/src/main/java/edu/dcc192/ex03/Usuario.java
+++ ex02/DCC192-2024.3/ex04/src/main/java/edu/dcc192/ex03/Usuario.java
@@ -1,6 +1,17 @@
 package edu.dcc192.ex03;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String login;
+    private String senha;
     private String nome;
 
     public String getNome() {
@@ -9,5 +20,43 @@ public class Usuario {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public Usuario(){
+        this(null,null);
+    }
+    
+    public Usuario(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    @Override
+    public String toString() {
+        return "Usuario [id=" + id + ", login=" + login + ", senha=" + senha + "]";
     }
 }
