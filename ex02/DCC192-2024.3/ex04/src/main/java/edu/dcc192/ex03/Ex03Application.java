8c8595b88ae4601497edc212fd605bffbdff1f8f
taíssa <ol.taissa.2002@gmail.com>
2025-01-09 16:13:14 -0300

    adiciona dados para tabela de usuários

--- ex02/DCC192-2024.3/ex04/src/main/java/edu/dcc192/ex03/Ex03Application.java
+++ ex02/DCC192-2024.3/ex04/src/main/java/edu/dcc192/ex03/Ex03Application.java
@@ -2,12 +2,19 @@ package edu.dcc192.ex03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Ex03Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ex03Application.class, args); 
-	}
+		ConfigurableApplicationContext ctx = SpringApplication.run(Ex03Application.class, args);
+	
+	UsuarioRepository rep = ctx.getBean(UsuarioRepository.class);
+		rep.save(new Usuario("ciro","1234"));
+		rep.save(new Usuario("jose","1234"));
+		rep.save(new Usuario("maria","1234"));
 
+		System.out.println(rep.findAll().toString());
+	}
 }
