b5eb37e341830c48b0bc5442f801b0489b91194d
taissaxl <taissa.leal@estudante.ufjf.br>
2025-01-09 22:26:11 -0300

    retira dados estáticos

--- ex02/DCC192-2024.3/ex04/src/main/java/edu/dcc192/ex03/Ex03Application.java
+++ ex02/DCC192-2024.3/ex04/src/main/java/edu/dcc192/ex03/Ex03Application.java
@@ -2,19 +2,11 @@ package edu.dcc192.ex03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Ex03Application {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(Ex03Application.class, args);
-	
-	UsuarioRepository rep = ctx.getBean(UsuarioRepository.class);
-		rep.save(new Usuario("ciro","1234"));
-		rep.save(new Usuario("jose","1234"));
-		rep.save(new Usuario("maria","1234"));
-
-		System.out.println(rep.findAll().toString());
+		SpringApplication.run(Ex03Application.class, args);
 	}
 }
