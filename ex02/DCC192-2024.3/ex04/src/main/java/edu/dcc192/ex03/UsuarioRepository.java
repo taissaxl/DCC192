8872c3d3d179d1bab7fa7a2c861208acd1cb4519
Taíssa <ol.taissa.2002@gmail.com>
2025-01-21 15:42:50 -0300

    add método de busca para verificação

--- ex02/DCC192-2024.3/ex04/src/main/java/edu/dcc192/ex03/UsuarioRepository.java
+++ ex02/DCC192-2024.3/ex04/src/main/java/edu/dcc192/ex03/UsuarioRepository.java
@@ -3,5 +3,6 @@ package edu.dcc192.ex03;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    
+        // Método para buscar um usuário pelo nome e senha
+        Usuario findByNomeAndSenha(String nome, String senha);
 }
